4f0ce42587d15cd6ac53c629e1a6f96cce6a1ccc
Dave Doyle <dave.s.doyle@gmail.com>
2009-06-07 01:58:36 -0400

    - refactor a bit - add some convenience methods - break the code... because I just want this committed tonight

--- wundercounter.php
+++ wundercounter.php
@@ -42,41 +42,40 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 
-
-/** This class wraps up lots of secret knowledge about how to make
- *  Wordpress "multi" widgets. These are widgets that allow more than one
- *  instance to be created. The standard "Text" widget is a simple example.
- *
- *  You must extend this class and over-ride three member functions.
- *  Scroll to the bottom of the file for a fully working example.
- */
-
 class WunderPluginBase {
     
-    var $base_path  = NULL;
-    var $base_url   = NULL;
+    var $plugin_url = '';
+    var $plugin_dir = '';
     
     // if your subclass has a method "register_hooks" it'll be auto-run
     // on instantiation.  We'll amend this in the PluginWidget class to also
     // automatically register the widget
-    var $call_on_instantiation = array('register_hooks');
+    var $run_on_construct = array('register_hooks');
     
     /**
      * Constructor
      *
      * @param   string      $plugin_path    The pathname to the plugin (just pass __FILE__ to the constructor)
      */
-    function WunderBase ($plugin_pathname) {
+    function WunderPluginBase ($plugin_file = '') {
         
-        $this->base_url = WP_PLUGIN_URL.'/'.str_replace(basename( $plugin_pathname ),"",plugin_basename( $plugin_pathname));
-        $this->base_path = WP_PLUGIN_DIR.'/'.str_replace(basename( $plugin_pathname ),"",plugin_basename( $plugin_pathname));
-        $this->base_path = preg_replace('/\\/','/',$this->base_path); # os-compat
+        if(!strlen($plugin_file))
+            die(__CLASS__ . ' requires a plugin file; just pass __FILE__');
         
-        // run the methods named in $this->call_on_instanciation if they exist
-        foreach ( $this->call_on_instantiation as $methodname ) {
-            if(method_exists($this,$methodname)) 
-                call_user_func($this, $methodname);
+        $this->plugin_url = WP_PLUGIN_URL.'/'.str_replace(basename( $plugin_file ),"",plugin_basename( $plugin_file )); ;
+        $this->plugin_dir = WP_PLUGIN_DIR . '/' . str_replace(basename( $plugin_file ),"",plugin_basename( $plugin_file ));
+        $this->plugin_dir = preg_replace('/\\/','/',$this->plugin_dir); # os-compat
+        
+        // if you defined a register_hooks method in a subclass we'll run it
+        // on instantiation
+
+        foreach ( $this->run_on_construct as $method ) {
+            if( method_exists($this,$method) ) {
+                call_user_func(array($this, $method)); // equivalent to $this->$method()
+                
+            }
         }
+        
     }
     
     /**
@@ -85,36 +84,80 @@ class WunderPluginBase {
      * @param   string      $path   (optional) path to append to the end of the url
      * @return  string              A URL relative to the plugin's path
      */
-    
-    function plugin_url($pathname = '') {
-        
-        // verify arg
-        if (!isset($pathname))
-            $pathname = '';
-        
-        // error check
-        if ( !is_string($pathname) )
-            die("pathname must be a string");
-        
-        $pathname = preg_replace('/^\/+/','',$pathname);
-        return $this->base_url . $pathname;
+    // lazy load the plugin's base url
+    function plugin_url($path = '', $relative = FALSE) {
+        if(!isset($path))
+            $path = '';
+        else            
+            $path = preg_replace('/^\/+/','',$path);
+            
+        $url = $this->plugin_url . $path;
+
+        // convert to a pseudo-relative path (maeaning I've pulled off the http and domain)
+        if($relative)
+            $url = preg_replace('/^[a-z]+:\/\/[^\/]+\//','/',$url);
+            
+        return $url;
     }
     
-    function plugin_dir($pathname = '') {
-        // verify arg
-        if (!isset($pathname))
-            $pathname = '';
-        
-        // error check
+    function plugin_dir($path = '') {
+        if(!isset($path))
+            $path = '';
+
         if ( !is_string($pathname) )
             die("pathname must be a string");
-        $pathname = preg_replace('/\\/','/',$pathname); # force forward slash for os-compat
-        $pathname = $this->base_dir . preg_replace('/^\/+/','',$pathname);
+
+        // force all slashes to be forward (os-compat) and remove leading slashes from path
+        $path = preg_replace(array('/\\/','/^\/+/'),array('/',''),$pathname);
+        return $this->plugin_dir . $path;
+    }
+ 
+    /* generate a <link> tag for css
+     * often used when putting a hook into wp_head filter (for adding css to blog pages)
+     * or admin_head (for adding css to admin pages)
+     */
+    function css_link_tag($css = '') {
+        if(!isset($css) || $css == '')
+            return '';
         
-        return $this->base_dir . $pathname;
+        return sprintf("<link type='text/css' rel='stylesheet' href='%s' />\n", $css);
+    }
+
+    // from http://ca2.php.net/manual/en/function.is-array.php#90929
+    // return tru if the array is associative (a perl hash)
+    function is_assoc(&$array) {
+        return (is_array($array) && 0 !== count(array_diff_key($array, array_keys(array_keys($array)))));
     }
     
+    function make_option_list(&$items = array(), $selected = '') {
+        if(!is_array($items) || !count($items) || !is_string($selected))
+            return '';
+        
+        $html = '';
+        $is_hash = $this->is_assoc($items);
+        
+        foreach ( $items as $key => $value ) {
+            // if items is indexed, the value and the label are the same
+            $val    = $is_hash ? $key : $value;
+            $label  = $value;
+            $html .= sprintf(
+                "<option value='%s' %s>%s</option>\n",
+                wp_specialchars($val),
+                ( $val == $selected ? 'selected="selected"' : ''),
+                wp_specialchars($label)
+            );
+        }
+        return $html;        
+    }
 }
+
+/** This class wraps up lots of secret knowledge about how to make
+ *  Wordpress "multi" widgets. These are widgets that allow more than one
+ *  instance to be created. The standard "Text" widget is a simple example.
+ *
+ *  You must extend this class and over-ride three member functions.
+ *  Scroll to the bottom of the file for a fully working example.
+ */
 
 class WunderPluginWidget extends WunderPluginBase
 {
@@ -128,7 +171,15 @@ class WunderPluginWidget extends WunderPluginBase
     
     var $number =false; ///< Unique ID number of the current instance.
     var $id =false; ///< Unique ID string of the current instance (id_base-number)
-    var $call_on_instantiation = array('register_hooks','_register');
+
+    /*
+     * list of methods to run on construct
+     * the _widgets_init automatically registers the widget and the widget control
+     *
+     * as per a non-widgetized plugin, register hooks is where you put calls to
+     * other add_hook and add_filter items
+     */
+    var $run_on_construct = array('_widgits_init','register_hooks');
 
 
     //
@@ -173,12 +224,13 @@ class WunderPluginWidget extends WunderPluginBase
         $plugin_pathname, // the full pathname of the main plugin file (just pass __file__)
         $id_base,         // a unique id by which this widget will be known
         $name,            // English name of the Widget
-        $widget_options = array(), //
-        $control_options = array()
+        $widget_options = array(), // usually just 'description'
+        $control_options = array() // widget control box special stuff (often just height and width)
     ) {
-        $this->id_base = $id_base;
-        $this->name = $name;
-        $this->option_name = 'multiwidget_'.$id_base;
+        $this->id_base      = $id_base; // a unique id for the plugin
+        $this->name         = $name; // name of the plugin as shown on the plugins screen
+        $this->option_name  = 'multiwidget_'.$id_base; // becomes the stored key in the options database for this widget
+        
         $this->widget_options = wp_parse_args(
             $widget_options,
             array('classname'=>$this->option_name)
@@ -401,6 +453,39 @@ class WunderCounter extends WunderPluginWidget {
         'page'      => '',
     );
     
+    var $styles = array(
+        'default'           => 'Default Style',
+        'odometer'          => 'Odometer',
+        '57chevy'           => '\'57 Chevy',
+        'odometerblack'     => 'Odometer (Black)',
+        'odometerwhite'     => 'Odometer (White)',
+        'brush'             => 'Brush',
+        'chalk'             => 'Chalk',
+        'nextgen'           => 'Next Generation',
+        'flame'             => 'Flame',
+        'katt151'           => 'Katt 151',
+        'microsc'           => 'Microscopic',
+        'miniscu'           => 'Miniscule',
+        'stencil'           => 'Stencil',
+        'punk'              => 'Punk',
+        'rosewd'            => 'Rosewood',
+    );
+    
+    var $background = array(
+        'transparent',
+        'black',
+        'white',
+        'red',
+        'gray',
+    );
+    
+    var $text_colour = array(
+        'black',
+        'white',
+        'red',
+        'gray',
+    );
+    
     function WunderCounter() {
         $this->WunderPluginWidget(
             __FILE__,
@@ -418,7 +503,7 @@ class WunderCounter extends WunderPluginWidget {
     
         
     function init() {
-        add_submenu_page('plugins.php','WunderCounter', 'WunderCounter', 10, __FILE__, array(&$this,'settings_menu'));
+        add_submenu_page('plugins.php','WunderCounter', 'WunderCounter', 10, __FILE__, array(&$this,'admin_menu'));
         
     }
 
@@ -434,7 +519,61 @@ class WunderCounter extends WunderPluginWidget {
         <?php
     }
     
-    function settings_page() {
+    function defaults($options) {
+        
+        // make sure we at least have something
+        if     ( !isset($options)    ) { $instance = array(); }
+        elseif ( !is_array($options) ) { $instance = (array) $instance; }
+
+
+        $defaults = array(
+            'username'      => '',
+            'type'          => 'invisible',
+            'style'         => 'default',
+            'complexity'    => 'simple',
+            'background'    => 'transparent',
+            'text_colour'   => 'black',
+            'simple_type'   => 'single_id', // single_id|track_all
+            'simple_id'     => 'my_blog',   // the "tag" under which the hit counter counts when single_id is chosen
+            'adv_base_id'   => 'my_blog',
+            // the order of when these checks are done is important
+            'adv_home_type' => 'single_id', // base|single_id|url|composed  is_front_page
+            'adv_home_id'   => 'home',
+            //is_page() // base|single_id|composed|url
+            //is_archive()  // base|single_id|composed|url
+            //is_search()   // base|single_id|url         
+            //is_single()   // base|single_id|composed|url
+            // default    // base|single_id|composed
+            
+        );
+
+        // any are FALSE set to default (since 0 is invalid for any numerics we're safe)        
+        foreach ($defaults as $key => $val ) {
+            // if key not exists, value isn't set or it evaluates to false, set the default
+            if( !( array_key_exists($key,$options) && isset($options[$key]) &&  $options[$key]) )
+                $options[$key] = $val;
+        }
+        
+        // MUST validate the style to ensure we have a safe file name to load
+        if ( !array_key_exists($options['style'],self::$styles) )
+            $options['style'] = $defaults['style'];
+            
+        // make sure we have a integer showposts
+        if (!is_int($options['showposts']))
+            $options['showposts'] = $defaults['showposts'];
+
+        // make sure categories is an array            
+        if (!is_array($options['categories']))
+            $options['categories'] = $defaults['categories'];
+            
+        return $instance;
+    }
+    
+    
+    function admin_page() {
+        
+        // get base options
+        $options = get_option($this->id_base);
         // IF UPDATE
         
         //DISPLAY
@@ -448,8 +587,11 @@ class WunderCounter extends WunderPluginWidget {
         <table class="form-table" style='width: auto;'>
 
             <tr valign="top">
-                <th scope="row" rowspan='3'>Username:<br /><span style='font-size: .8em'>(this is your WunderCounter user name; this plugin will not function without a username)</span></th>
+                <th scope="row">Username:<br /><span style='font-size: .8em'>(this is your WunderCounter user name; this plugin will not function without a username)</span></th>
                 <td colspan='2'>
+                    <input type='text' name='username' id='username'
+                </td>
+            </tr>
                     
             <tr valign="top">
                 <th scope="row" rowspan='3'>Type of Counter:</th>
@@ -475,37 +617,46 @@ class WunderCounter extends WunderPluginWidget {
                 </td>
                 <td>
                     Visible Counter (manual)<br />
-                    <span style='font-size: .8em;'>You will place the WunderCounter Widget into the sidesbars manually.</span>
+                    <span style='font-size: .8em;'>You must place the WunderCounter Widget into the sidebars manually.</span>
                 </td>
             </tr>
 
             <tr valign="top">
-                <th scope="row">Style:<br /><span style='font-size: .8em'>(only applies when using the visible counter)</span></th>
+                <th scope="row">Style:<br /><span style='font-size: .8em'>(only applies when using the 'default' style)</span></th>
                 <td colspan='2'>
-                    <select name='style' id='style'>
-                        <option value=''>Default Style</option>
-                        <option value='odometer'>Odometer</option>
-                        <option value='57chevy'>&rsquo;57 Chevy</option>
-                        <option value='odometerblack'>Odometer (Black)</option>
-                        <option value='odometerwhite'>Odometer (White)</option>
-                        <option value='brush'>Brush</option>
-                        <option value='chalk'>Chalk</option>
-                        <option value='nextgen'>Next Generation</option>
-                        <option value='flame'>Flame</option>
-                        <option value='katt151'>Katt 151</option>
-                        <option value='microsc'>Microscopic</option>
-                        <option value='miniscu'>Miniscule</option>
-                        <option value='stencil'>Stencil</option>
-                        <option value='punk'>Punk</option>
-                        <option value='rosewd'>Rosewd</option>
+                    <select name='background' id='background'>
+                        <?php echo $this->make_option_list($this->background,''); ?>
+                    </select>
+                </td>
+            </tr>
+            
+            <tr valign='top' id='background-color-row'>
+                <th scope='row'>Background Color</th>
+                <td colspan='2'>
+                    <select name='text_colour' id='text_colour'>
+                        <?php echo $this->make_option_list($this->text_colour,''); ?>
                     </select>
                 </td>
             </tr>
     
+            <tr valign='top'>
+                <th rowspan='2' scope='row'>Settings Level:</th>
+                <td><input type='radio' name='complexity' value='simple'></td>
+                <td>Simple</td>
+            </tr>
+    
+            <tr valign='top'>
+                <th rowspan='2' scope='row'>Settings Level:</th>
+                <td><input type='radio' name='complexity' value='advanced'></td>
+                <td>Advanced</td>
+            </tr>
+    
         </table>
     
+        
+    
         <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="new_option_name,some_other_option,option_etc" />
+        <!-- <input type="hidden" name="page_options" value="new_option_name,some_other_option,option_etc" /> -->
     
         <p class="submit">
             <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
