a581e7fa43534f5412f3516f1e0dca744f156450
Dave Doyle <ddoyle@oanda.com>
2009-06-11 11:04:20 -0400

    add the js for show/hiding simple/advanced settings

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function($){
-    
+    $('#wundercounter-complexity-advanced,#wundercounter-complexity-simple').change(function() {
+        //var $which = $("#wundercounter-complexity-advanced:checked").val() ? 'advanced' : 'simple';
+        $('#wundercounter-advanced,#wundercounter-simple').toggle();
+    });
  });  
