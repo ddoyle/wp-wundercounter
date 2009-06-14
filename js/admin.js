5a9098a2e474325dbfa530d0610932ff7759465e
Dave Doyle <dave.s.doyle@gmail.com>
2009-06-14 16:18:33 -0400

    js to enable/disable some advanced options text boxes

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,18 @@
 jQuery(document).ready(function($){
+    
     $('#wundercounter-complexity-advanced,#wundercounter-complexity-simple').change(function() {
         //var $which = $("#wundercounter-complexity-advanced:checked").val() ? 'advanced' : 'simple';
         $('#wundercounter-advanced,#wundercounter-simple').toggle();
+    });
+    
+    
+    $('#adv_home,#adv_page,#adv_archive,#adv_search,#adv_post,#adv_default').change(function() {
+        var id = '#' + $(this).attr('id') + '_id';
+        if ($(this).val() == 'simple') {
+            $(id).attr('disabled',false).css('background-color','#FFF');
+        }
+        else {
+            $(id).attr('disabled','true').css('backgrond-color','#999');
+        }
     });
  });  
